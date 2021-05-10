9d0bd8dedb9204f89beb894a43d5a0c46b4499d5
hanzawa.s <hanzawa.s@gos-pa.co.jp>
2021-05-10 13:56:53 +0900

    add create

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -11,14 +11,14 @@
                 <div class="card-body">
                     <input type="hidden" name="id" value={{ $value['id'] }}>
                     <h6>Date</h6>
-                    <input type="datetime-local" name="start_date" value="{{ $value['start_date'] }}">~
-                    <input type="datetime-local" name="end_date" value="{{ $value['end_date'] }}"><br><br>
+                    <input type="datetime-local" name="start_date" value="{{ old('start_date', $value['start_date']) }}">~
+                    <input type="datetime-local" name="end_date" value="{{ old('end_date', $value['end_date']) }}"><br><br>
 
                     <h6>Title</h6>
-                    <input type="text" name="title" value="{{ $value['title'] }}"></input><br><br>
+                    <input type="text" name="title" value="{{ old('title', $value['title']) }}"></input><br><br>
 
                     <h6>Schedule</h6>
-                    <textarea name="schedule" cols="30" rows="10" value="">{{ $value['schedule'] }}</textarea><br><br>
+                    <textarea name="schedule" cols="30" rows="10" value="">{{ old('schedule',$value['schedule']) }}</textarea><br><br>
 
                     <input class="btn btn-outline-secondary" type="submit" formaction="/store" value="save">
                     <input class="btn btn-outline-secondary" type="submit" formaction="/delete" value="delete">
